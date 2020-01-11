1a903c448a0fec8f132c2b814d3d1caa4134f4cb
Zach Bolton <zachb@zachs-mac-mini.local>
2020-01-11 11:44:29 -0800

    Initial Commit

---
+++ raceclasscolorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raceclasscolorer
+//
+//  Created by Zach Bolton on 1/11/20.
+//  Copyright © 2020 Zach Bolton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
