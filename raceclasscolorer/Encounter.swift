f60e4a46d19682f44f0c1561e87f9d61a0f02bb1
Zach Bolton <zachb@Zachs-Mac-mini.local>
2020-02-28 08:14:07 -0800

    Battle ground exists but is kind of backwards

--- raceclasscolorer/Encounter.swift
+++ raceclasscolorer/Encounter.swift
@@ -8,31 +8,110 @@
 
 import UIKit
 
+enum Spot {
+    case void
+    case empty
+    case terain(String)
+    case person(Person)
+}
+
+class BattleGround {
+    private(set) var ourTeamSide: [Spot]
+    private(set) var enemyTeamSide: [Spot]
+    
+    init(ourTeam: Team, enemyTeam: Team) {
+        ourTeamSide = [Spot]()
+        ourTeamSide.append(.terain("Tree"))
+        ourTeamSide.append(contentsOf: ourTeam.members.map({ Spot.person($0) }))
+        ourTeamSide.append(.empty)
+        ourTeamSide.append(.empty)
+        ourTeamSide.append(.terain("Rock"))
+        
+        enemyTeamSide = [Spot]()
+        enemyTeamSide.append(.terain("Wall"))
+        enemyTeamSide.append(contentsOf: enemyTeam.members.map({ Spot.person($0) }))
+        enemyTeamSide.append(.empty)
+        enemyTeamSide.append(.empty)
+        enemyTeamSide.append(.terain("Rock"))
+    }
+    
+    func closestEnemy(attackingPerson: Person, onOur: Bool) -> Person? {
+        let attackingTeam = onOur ? ourTeamSide : enemyTeamSide
+        let defendingTeam = onOur ? enemyTeamSide : ourTeamSide
+        
+        //TODO: There has to be a better way then this messy switch right?
+        guard let ourSpot = attackingTeam.firstIndex(where: { (spot) in
+            switch spot {
+            case .person(let personInSpot):
+                if personInSpot.id == attackingPerson.id {
+                    return true
+                }
+            default:
+                break
+            }
+            return false
+        }) else {
+            print("Looked for a person not on our side")
+            return nil
+        }
+
+        let closetSpot = defendingTeam[ourSpot]
+        
+        switch closetSpot {
+        case .person(let personInSpot):
+            return personInSpot
+        default:
+            break
+        }
+        
+        return nil
+    }
+}
+
 struct Encounter {
     let ourTeam: Team
     let enemyTeam: Team
     
+    var battleGround: BattleGround
+    
+    init(ourTeam: Team, enemyTeam: Team) {
+        self.ourTeam = ourTeam
+        self.enemyTeam = enemyTeam
+        
+        self.battleGround = BattleGround(ourTeam: ourTeam, enemyTeam: enemyTeam)
+    }
+    
+    private func teamAttack(attackingTeam: Team, defendingTeam: Team, isOurTeam: Bool) {
+        attackingTeam.members.enumerated().forEach({ (index, member) in
+            guard member.currentHp > 0 else {
+                return
+            }
+            
+            switch member.attackType {
+            case .aoe:
+                for enemy in defendingTeam.members {
+                    enemy.currentHp -= Int(Float(2) * member.attackModifer(enemy: enemy))
+                }
+            case .ping:
+                let enemy = defendingTeam.members.randomElement()!
+                enemy.currentHp -= 1
+            case .single:
+                if let enemy = battleGround.closestEnemy(attackingPerson: member, onOur: isOurTeam) {
+                    enemy.currentHp -= Int(Float(5) * member.attackModifer(enemy: enemy))
+                }
+            case .buff:
+                for member in attackingTeam.members {
+                    member.currentAttack += 1
+                }
+            }
+        })
+    }
+    
     func fight() -> FightState {
         
         while ourTeam.isAlive && enemyTeam.isAlive {
-            ourTeam.members.enumerated().forEach({ (index, member) in
-                switch member.attackType {
-                case .aoe:
-                    for enemy in enemyTeam.members {
-                        enemy.currentHp -= Int(Float(2) * member.attackModifer(enemy: enemy))
-                    }
-                case .ping:
-                    let enemy = enemyTeam.members.randomElement()!
-                    enemy.currentHp -= 1
-                case .single:
-                    let enemy = enemyTeam.members[index]
-                    enemy.currentHp -= Int(Float(5) * member.attackModifer(enemy: enemy))
-                case .buff:
-                    for member in ourTeam.members {
-                        member.currentAttack += 1
-                    }
-                }
-            })
+            teamAttack(attackingTeam: ourTeam, defendingTeam: enemyTeam, isOurTeam: true)
+            teamAttack(attackingTeam: enemyTeam, defendingTeam: ourTeam, isOurTeam: false)
         }
         
         let wonState = ourTeam.isAlive
--- raceclasscolorer/ViewController.swift
+++ raceclasscolorer/ViewController.swift
@@ -13,52 +13,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var enemyTeamView: UIStackView!
     @IBOutlet weak var ourTeamView: UIStackView!
     
-    var ourTeam: Team?
-    var enemyTeam: Team?
+    //TODO: can I fix this?
+    var ourTeam: Team!
+    var enemyTeam: Team!
+    var encounter: Encounter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //need a race/class by id
-        let human1 = Person(preRace: allRaces[0], preClass: allClasses[0], preColor: .red)
-        let human2 = Person(preRace: allRaces[0], preClass: allClasses[1], preColor: .blue)
+        let human1 = Person(preRace: allRaces[0], preClass: allClasses[2], preColor: .red)
+        let human2 = Person(preRace: allRaces[0], preClass: allClasses[2], preColor: .blue)
         
         let humanTeam = Team(members: [human1, human2])
         //print(human1.totalBuffs(team: humanTeam))
         
-        let elf1 = Person(preRace: allRaces[1], preClass: allClasses[0], preColor: .green)
+        let elf1 = Person(preRace: allRaces[1], preClass: allClasses[2], preColor: .green)
         humanTeam.members.append(elf1)
         //print(human1.totalBuffs(team: humanTeam))
         
         ourTeam = humanTeam
-        ourTeamView.show(team: humanTeam)
         
         let otherTeam = Team(members: [Person(), Person(), Person()])
         enemyTeam = otherTeam
-        enemyTeamView.show(team: otherTeam)
+        
+        encounter = Encounter(ourTeam: ourTeam, enemyTeam: enemyTeam)
+
+        ourTeamView.show(spots: encounter.battleGround.ourTeamSide, team: humanTeam)
+        enemyTeamView.show(spots: encounter.battleGround.enemyTeamSide, team: otherTeam)
     }
     
     @IBAction func attack(_ sender: Any) {
-        guard let ourTeam = ourTeam, let enemyTeam = enemyTeam else {
+        guard let encounter = encounter else {
             return
         }
         
-        let encounter = Encounter(ourTeam: ourTeam, enemyTeam: enemyTeam)
         encounter.fight()
     }
 }
 
 extension UIStackView {
-    func show(team: Team) {
+    //TODO: make combo view and show BattleGround?
+    func show(spots: [Spot], team: Team) {
         arrangedSubviews.forEach({ $0.removeFromSuperview() })
         
-        team.members.forEach({
+        spots.forEach({ (spot) in
             let label = UILabel()
             label.numberOfLines = 0
             label.textAlignment = .center
-            label.backgroundColor = .lightGray
             
-            label.text = "\($0.race.name)\n\($0.pclass.name)\n\($0.color)\n\($0.hp) \($0.attack) \($0.defense)\n\($0.totalBuffs(team: team))"
+            switch spot {
+            case .void:
+                label.backgroundColor = .black
+                label.text = "v"
+            case .empty:
+                label.backgroundColor = .lightGray
+                label.text = "e"
+            case .terain(let name):
+                label.backgroundColor = .lightGray
+                label.text = name
+            case .person(let personInSpot):
+                label.backgroundColor = .lightGray
+                
+                label.text = "\(personInSpot.race.name)\n\(personInSpot.pclass.name)\n\(personInSpot.color)\n\(personInSpot.hp) \(personInSpot.attack) \(personInSpot.defense)\n\(personInSpot.totalBuffs(team: team))"
+            }
             
             addArrangedSubview(label)
         })
